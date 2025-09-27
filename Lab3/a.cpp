46b4e1b0ce2a16ff200c7c6cd92fce019ad14ef9
MereilimSerikkali <serikkalimereilim@gmail.com>
2025-09-27 08:56:14 +0500

    yay

--- Lab3/a.cpp
+++ Lab3/a.cpp
@@ -2,21 +2,7 @@
 #include <vector>
 using namespace std;
 
-int binarySearchRow(const vector<int>& row, int x) {
-    int l = 0, r = (int)row.size() - 1;
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        if (row[mid] == x) return mid;
-        else if (row[mid] < x) l = mid + 1;
-        else r = mid - 1;
-    }
-    return -1;
-}
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int t;
     cin >> t;
 
@@ -36,12 +22,13 @@ int main() {
     for (int q = 0; q < t; q++) {
         int x = queries[q];
         bool found = false;
-
         for (int i = 0; i < n && !found; i++) {
-            int j = binarySearchRow(arr[i], x);
-            if (j != -1) {
-                cout << i << " " << j << "\n";
-                found = true;
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == x) {
+                    cout << i << " " << j << "\n";
+                    found = true;
+                    break;
+                }
             }
         }
         if (!found) cout << -1 << "\n";
@@ -49,4 +36,3 @@ int main() {
 
     return 0;
 }
-
--- Lab3/c.cpp
+++ Lab3/c.cpp
@@ -3,51 +3,52 @@
 #include <algorithm>
 using namespace std;
 
-int firstGreaterEqual(const vector<long long>& a, long long x) {
+int firstGreaterEqual(const vector<int>& a, int x) {
     int l = 0, r = (int)a.size();
     while (l < r) {
-        int mid = (l + r) / 2;
+        int mid = (l + r) / 2; // divide by 2
         if (a[mid] < x) l = mid + 1;
         else r = mid;
     }
     return l;
 }
 
-int firstGreater(const vector<long long>& a, long long x) {
+int firstGreater(const vector<int>& a, int x) {
     int l = 0, r = (int)a.size();
     while (l < r) {
-        int mid = (l + r) / 2;
+        int mid = (l + r) / 2; 
         if (a[mid] <= x) l = mid + 1;
         else r = mid;
     }
     return l;
 }
 
-int countRange(const vector<long long>& a, long long L, long long R) {
+int countRange(const vector<int>& a, int L, int R) {
     if (L > R) return 0;
     return firstGreater(a, R) - firstGreaterEqual(a, L);
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int n, q;
     cin >> n >> q;
 
-    vector<long long> a(n);
+    vector<int> a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
     sort(a.begin(), a.end());
 
     while (q--) {
-        long long l1, r1, l2, r2;
+        int l1, r1, l2, r2;
         cin >> l1 >> r1 >> l2 >> r2;
 
         int c1 = countRange(a, l1, r1);
         int c2 = countRange(a, l2, r2);
 
-        cout << (c1 + c2) << "\n";
-    }
+        // overlap
+        int L = max(l1, l2);
+        int R = min(r1, r2);
+        int cOverlap = countRange(a, L, R);
 
+        cout << (c1 + c2 - cOverlap) << "\n";
+    }
     return 0;
 }
