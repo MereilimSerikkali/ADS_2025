2dcd6d1a22ee79f3ed7c658f4f094bec5ba9428c
MereilimSerikkali <serikkalimereilim@gmail.com>
2025-11-02 23:32:46 +0500

    changed it

--- Lab8/a.cpp
+++ Lab8/a.cpp
@@ -1,69 +1,57 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
-
 using namespace std;
 
 vector<string> a;
-unordered_set<string> set;
+unordered_set<string> setList;
 
-long long powerOfNumber(long long n, long long power){
-    if(power == 0) return 1;
-    if(power == 1) return n;
-    long long result = 1;
-    for(int i = 0; i < power; i++){
-        result *= 11;
-        result %= (1000000007);
-    }
-    return result;
-}
+string calculateHash(const string &s) {
+    const long long MOD = 1000000007;
+    const long long BASE = 11;
 
-string calculateHash(string s){
     long long h = 0;
-    for(int i = 0; i < s.size(); i++){
-        long long temp = (((long long)(s[i]) - 47) % (1000000007)) * (powerOfNumber(11, i));
-        h += temp;
-        h %= (1000000007);
+    long long power = 1;
+
+    for (int i = 0; i < (int)s.size(); i++) {
+        long long temp = ((long long)(s[i]) - 47) * power;
+        temp %= MOD;
+        h = (h + temp) % MOD;
+        power = (power * BASE) % MOD;
     }
 
+    if (h < 0) h += MOD;
     return to_string(h);
 }
 
-bool findHashFromInput(string s){
-    // for(int i = 0; i < a.size(); i++){
-    //     if(s == a[i]){
-    //         return true;
-    //     }
-    // }
-    // return false;
-
-    // time complexity O(1)
-    if (set.find(s) != set.end()) {
-        return true;
-    } else {
-        return false;
-    }
+bool findHashFromInput(const string &s) {
+    return setList.find(s) != setList.end();
 }
 
-int main(){
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-    for(int i = 0; i < n*2; i++){
+
+    for (int i = 0; i < n * 2; i++) {
         string s;
         cin >> s;
-
         a.push_back(s);
-        set.insert(s);
+        setList.insert(s);
     }
 
     int cnt = 0;
 
-    for(int i = 0; i < a.size(); i++){
-        if(cnt == n) break;
+    for (int i = 0; i < (int)a.size(); i++) {
+        if (cnt == n) break;
         string hashCode = calculateHash(a[i]);
-        if(findHashFromInput(hashCode)){
-            cout << "Hash of string \"" << a[i] << "\"" << " is " << hashCode << endl;
+        if (findHashFromInput(hashCode)) {
+            cout << "Hash of string \"" << a[i] << "\" is " << hashCode << endl;
             cnt++;
         }
     }
+
+    return 0;
 }
